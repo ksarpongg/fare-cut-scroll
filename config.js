e7764ae367a87aba4bc073ecc86664126924f617
ksarpongg <140520269+ksarpongg@users.noreply.github.com>
2025-07-01 13:16:07 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,160 +1,18 @@
+// config.js
 const config = {
   style: 'mapbox://styles/mapbox/satellite-v9',
-  accessToken: 'pk.eyJ1Ijoic2FycG9uZ2ciLCJhIjoiY21jaTdmdWJrMGN2bDJqcGR3ZnVhNGFnOSJ9.X5uC7PYBBzIjG_6bQ6184w',
+  accessToken: 'pk.eyJ1Ijoic2FycG9uZ2ciLCJhIjoiY21jaTdmdWJrMGN2bDJqcGR3ZnVhNGFnOSJ9.X5uC7PYBBzIjG_6bQ6184w', // your actual token
   chapters: [
-    {
-      id: 'slide1',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-1.0232, 7.9465], // Ghana national view
-        zoom: 6,
-        pitch: 0,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide2',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.1870, 5.6037], // Greater Accra region view
-        zoom: 9,
-        pitch: 0,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide3',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.423756, 5.534052], // Kasoa Bus Stop
-        zoom: 16,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide4',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.3920, 5.5470], // Kasoa to Old Barrier
-        zoom: 15,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide5',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.329034, 5.551157], // Old Barrier
-        zoom: 16,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide6',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.286262, 5.569050], // Old Barrier to Mallam
-        zoom: 15,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide7',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.286262, 5.569050], // Mallam
-        zoom: 16,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide8',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.237506, 5.566120], // Mallam to Kaneshie
-        zoom: 15,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide9',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.237506, 5.566120], // Kaneshie
-        zoom: 16,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide10',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.216840, 5.569132], // Kaneshie to Circle
-        zoom: 15,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    },
-    {
-      id: 'slide11',
-      alignment: 'center',
-      title: '',
-      description: '',
-      location: {
-        center: [-0.216840, 5.569132], // Circle
-        zoom: 16,
-        pitch: 60,
-        bearing: 0
-      },
-      onChapterEnter: [],
-      onChapterExit: []
-    }
+    { id: 'slide1', alignment: 'center', location: { center: [-1.0232, 7.9465], zoom: 6, pitch: 0, bearing: 0 } },
+    { id: 'slide2', alignment: 'center', location: { center: [-0.1870, 5.6037], zoom: 9, pitch: 0, bearing: 0 } },
+    { id: 'slide3', alignment: 'center', location: { center: [-0.423756, 5.534052], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide4', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 15, pitch: 60, bearing: 0 } },
+    { id: 'slide5', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide6', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 15, pitch: 60, bearing: 0 } },
+    { id: 'slide7', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide8', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 15, pitch: 60, bearing: 0 } },
+    { id: 'slide9', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide10', alignment: 'center', location: { center: [-0.216840, 5.569132], zoom: 15, pitch: 60, bearing: 0 } },
+    { id: 'slide11', alignment: 'center', location: { center: [-0.216840, 5.569132], zoom: 16, pitch: 60, bearing: 0 } }
   ]
 };
