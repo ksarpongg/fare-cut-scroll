c03e6cc81e3d11690a28f8b4cf3764947e8d6fe2
ksarpongg <140520269+ksarpongg@users.noreply.github.com>
2025-07-01 14:59:54 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ const config = {
   chapters: [
     { id: 'slide1', alignment: 'center', location: { center: [-1.0232, 7.9465], zoom: 6, pitch: 0, bearing: 0 } },
     { id: 'slide2', alignment: 'center', location: { center: [-0.22, 5.65], zoom: 9, pitch: 0, bearing: 0 } },
-    { id: 'slide3', alignment: 'center', location: { center: [-0.423756, 5.534052], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide3', alignment: 'center', location: { center: [-0.423756, 5.534052], zoom: 16, pitch: 0, bearing: 0 } },
     { id: 'slide4', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide5', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide5', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 16, pitch: 0, bearing: 0 } },
     { id: 'slide6', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide7', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide7', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 16, pitch: 0, bearing: 0 } },
     { id: 'slide8', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide9', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 16, pitch: 60, bearing: 0 } },
+    { id: 'slide9', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 16, pitch: 0, bearing: 0 } },
     { id: 'slide10', alignment: 'center', location: { center: [-0.21684, 5.569132], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide11', alignment: 'center', location: { center: [-0.21684, 5.569132], zoom: 16, pitch: 60, bearing: 0 } }
+    { id: 'slide11', alignment: 'center', location: { center: [-0.21684, 5.569132], zoom: 16, pitch: 0, bearing: 0 } }
   ]
 };
