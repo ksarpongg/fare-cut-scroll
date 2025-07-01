bf555a43af4d7a9930c4aec19096aebfb13e2559
ksarpongg <140520269+ksarpongg@users.noreply.github.com>
2025-07-01 14:24:57 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,115 @@ const config = {
   style: 'mapbox://styles/mapbox/satellite-v9',
   accessToken: 'pk.eyJ1Ijoic2FycG9uZ2ciLCJhIjoiY21jaTdmdWJrMGN2bDJqcGR3ZnVhNGFnOSJ9.X5uC7PYBBzIjG_6bQ6184w',
   chapters: [
-    { id: 'slide1', alignment: 'center', location: { center: [-1.0232, 7.9465], zoom: 6, pitch: 0, bearing: 0 } },
-    { id: 'slide2', alignment: 'center', location: { center: [-0.22, 5.65], zoom: 9, pitch: 0, bearing: 0 } },
-    { id: 'slide3', alignment: 'center', location: { center: [-0.423756, 5.534052], zoom: 16, pitch: 60, bearing: 0 } },
-    { id: 'slide4', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide5', alignment: 'center', location: { center: [-0.329034, 5.551157], zoom: 16, pitch: 60, bearing: 0 } },
-    { id: 'slide6', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide7', alignment: 'center', location: { center: [-0.286262, 5.569050], zoom: 16, pitch: 60, bearing: 0 } },
-    { id: 'slide8', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide9', alignment: 'center', location: { center: [-0.237506, 5.566120], zoom: 16, pitch: 60, bearing: 0 } },
-    { id: 'slide10', alignment: 'center', location: { center: [-0.21684, 5.569132], zoom: 15, pitch: 60, bearing: 0 } },
-    { id: 'slide11', alignment: 'center', location: { center: [-0.21684, 5.569132], zoom: 16, pitch: 60, bearing: 0 } }
+    {
+      id: 'slide1',
+      alignment: 'center',
+      location: {
+        center: [-1.0232, 7.9465],
+        zoom: 6,
+        pitch: 0,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide2',
+      alignment: 'center',
+      location: {
+        center: [-0.22, 5.65],
+        zoom: 9,
+        pitch: 0,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide3',
+      alignment: 'center',
+      location: {
+        center: [-0.423756, 5.534052], // Kasoa
+        zoom: 18.5,
+        pitch: 60,
+        bearing: -20
+      }
+    },
+    {
+      id: 'slide4',
+      alignment: 'center',
+      location: {
+        center: [-0.329034, 5.551157], // Old Barrier
+        zoom: 18.2,
+        pitch: 60,
+        bearing: -30
+      }
+    },
+    {
+      id: 'slide5',
+      alignment: 'center',
+      location: {
+        center: [-0.329034, 5.551157],
+        zoom: 18.5,
+        pitch: 60,
+        bearing: -10
+      }
+    },
+    {
+      id: 'slide6',
+      alignment: 'center',
+      location: {
+        center: [-0.286262, 5.569050], // Mallam
+        zoom: 18.2,
+        pitch: 60,
+        bearing: -15
+      }
+    },
+    {
+      id: 'slide7',
+      alignment: 'center',
+      location: {
+        center: [-0.286262, 5.569050],
+        zoom: 18.5,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide8',
+      alignment: 'center',
+      location: {
+        center: [-0.237506, 5.566120], // Kaneshie
+        zoom: 18.2,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide9',
+      alignment: 'center',
+      location: {
+        center: [-0.237506, 5.566120],
+        zoom: 18.5,
+        pitch: 60,
+        bearing: 15
+      }
+    },
+    {
+      id: 'slide10',
+      alignment: 'center',
+      location: {
+        center: [-0.21684, 5.569132], // Circle
+        zoom: 18.2,
+        pitch: 60,
+        bearing: 15
+      }
+    },
+    {
+      id: 'slide11',
+      alignment: 'center',
+      location: {
+        center: [-0.21684, 5.569132],
+        zoom: 18.5,
+        pitch: 60,
+        bearing: 30
+      }
+    }
   ]
 };
