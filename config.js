765686c4e4b71200f44a88e9bf9c4f229c8a959f
ksarpongg <140520269+ksarpongg@users.noreply.github.com>
2025-07-01 13:37:22 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js
 const config = {
   style: 'mapbox://styles/mapbox/satellite-v9',
   accessToken: 'pk.eyJ1Ijoic2Fyc2FycG9uZ2ciLCJhIjoiY21jaTdmdWJrMGN2bDJqcGR3ZnVhNGFnOSJ9.X5uC7PYBBzIjG_6bQ6184w',
@@ -7,7 +6,7 @@ const config = {
       id: 'slide1',
       alignment: 'center',
       location: {
-        center: [-1.0232, 7.9465], // Ghana national view
+        center: [-1.0232, 7.9465],
         zoom: 6,
         pitch: 0,
         bearing: 0
@@ -17,7 +16,7 @@ const config = {
       id: 'slide2',
       alignment: 'center',
       location: {
-        center: [-0.22, 5.65], // Greater Accra Region
+        center: [-0.22, 5.65],
         zoom: 9,
         pitch: 0,
         bearing: 0
@@ -27,7 +26,7 @@ const config = {
       id: 'slide3',
       alignment: 'center',
       location: {
-        center: [-0.423756, 5.534052], // Kasoa
+        center: [-0.423756, 5.534052],
         zoom: 16,
         pitch: 60,
         bearing: 0
@@ -37,7 +36,7 @@ const config = {
       id: 'slide4',
       alignment: 'center',
       location: {
-        center: [-0.329034, 5.551157], // Old Barrier
+        center: [-0.329034, 5.551157],
         zoom: 15,
         pitch: 60,
         bearing: 0
@@ -47,7 +46,7 @@ const config = {
       id: 'slide5',
       alignment: 'center',
       location: {
-        center: [-0.329034, 5.551157], // Old Barrier (repeat zoom)
+        center: [-0.329034, 5.551157],
         zoom: 16,
         pitch: 60,
         bearing: 0
@@ -55,4 +54,63 @@ const config = {
     },
     {
       id: 'slide6',
-      alignment: 'cent
+      alignment: 'center',
+      location: {
+        center: [-0.286262, 5.569050],
+        zoom: 15,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide7',
+      alignment: 'center',
+      location: {
+        center: [-0.286262, 5.569050],
+        zoom: 16,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide8',
+      alignment: 'center',
+      location: {
+        center: [-0.237506, 5.566120],
+        zoom: 15,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide9',
+      alignment: 'center',
+      location: {
+        center: [-0.237506, 5.566120],
+        zoom: 16,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide10',
+      alignment: 'center',
+      location: {
+        center: [-0.216840, 5.569132],
+        zoom: 15,
+        pitch: 60,
+        bearing: 0
+      }
+    },
+    {
+      id: 'slide11',
+      alignment: 'center',
+      location: {
+        center: [-0.216840, 5.569132],
+        zoom: 16,
+        pitch: 60,
+        bearing: 0
+      }
+    }
+  ]
+};
