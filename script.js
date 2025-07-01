54b2a2af8b418a15dea2dfbf4ef6383b99a7262d
ksarpongg <140520269+ksarpongg@users.noreply.github.com>
2025-07-01 13:20:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,54 +5,61 @@ const map = new mapboxgl.Map({
   style: config.style,
   center: config.chapters[0].location.center,
   zoom: config.chapters[0].location.zoom,
-  pitch: config.chapters[0].location.pitch || 0,
-  bearing: config.chapters[0].location.bearing || 0
+  pitch: config.chapters[0].location.pitch,
+  bearing: config.chapters[0].location.bearing
 });
 
-// Add Ghana border outline on map load
-map.on('load', function () {
-  // === Ghana border outline ===
+let accraMarker;
+
+map.on('load', () => {
+  // Ghana border
   map.addSource('ghana-border', {
     type: 'geojson',
     data: 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries/GHA.geo.json'
   });
-
   map.addLayer({
     id: 'ghana-outline',
     type: 'line',
     source: 'ghana-border',
-    paint: {
-      'line-color': '#ffffff',
-      'line-width': 2
-    }
+    paint: { 'line-color': '#fff', 'line-width': 2 }
   });
 
-  // === Create blinking marker for Greater Accra ===
-  const accraMarkerEl = document.createElement('div');
-  accraMarkerEl.className = 'pulse-marker';
+  // Static bus stop markers
+  const stops = [
+    { name: 'Kasoa', coords: [-0.423756, 5.534052] },
+    { name: 'Old Barrier', coords: [-0.329034, 5.551157] },
+    { name: 'Mallam', coords: [-0.286262, 5.569050] },
+    { name: 'Kaneshie', coords: [-0.237506, 5.566120] },
+    { name: 'Circle', coords: [-0.21684, 5.569132] }
+  ];
+  stops.forEach(s => {
+    const el = document.createElement('div');
+    el.className = 'stop-marker';
+    el.innerText = s.name;
+    new mapboxgl.Marker(el).setLngLat(s.coords).addTo(map);
+  });
 
-  // Fix: Add marker with offset to avoid top-left bug
-  window.accraMarker = new mapboxgl.Marker({
-    element: accraMarkerEl,
-    offset: [0, 0]
-  }).setLngLat([-0.22, 5.65]); // Coordinates of Greater Accra
+  // Blinking Greater Accra marker
+  const el2 = document.createElement('div');
+  el2.className = 'pulse-marker';
+  accraMarker = new mapboxgl.Marker({ element: el2, offset: [0,0] })
+    .setLngLat([-0.22, 5.65]);
 });
 
-// === Add static bus stop labels ===
-const busStops = [
-  { name: 'Kasoa', coords: [-0.423756, 5.534052] },
-  { name: 'Old Barrier', coords: [-0.329034, 5.551157] },
-  { name: 'Mallam', coords: [-0.286262, 5.569050] },
-  { name: 'Kaneshie', coords: [-0.237506, 5.566120] },
-  { name: 'Circle', coords: [-0.216840, 5.569132] }
-];
-
-busStops.forEach(stop => {
-  const stopEl = document.createElement('div');
-  stopEl.className = 'stop-marker';
-  stopEl.innerText = stop.name;
-  new mapboxgl.Marker(stopEl).setLngLat(stop.coords).addTo(map);
-});
-
-// === Scrollama Scroll Logic ===
 const scroller = scrollama();
+scroller.setup({ step: '.step', offset: 0.5, debug: false })
+  .onStepEnter(resp => {
+    const ch = config.chapters.find(c => c.id === resp.element.id);
+    if (ch && ch.location) {
+      map.flyTo({
+        center: ch.location.center,
+        zoom: ch.location.zoom,
+        pitch: ch.location.pitch,
+        bearing: ch.location.bearing,
+        speed: 0.8,
+        essential: true
+      });
+    }
+    // Show only on slide2
+    resp.element.id === 'slide2' ? accraMarker.addTo(map) : accraMarker.remove();
+  });
